89fa8b799e4e6ba081dafdb90c2afc7a1daf379e
Prof. Otávio Medeiros <prof.otavio.medeiros@gmail.com>
2022-10-22 10:38:19 -0300

    main > commit 002 > 2022-10-22 10:38:19 -03:00:00

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,26 @@
+let gammer = "X";
+
+function winner() {
+  let elements = document.getElementById("tabuleiro").children;
+  if (
+    elements[0].innerText === elements[1].innerText &&
+    elements[1].innerText === elements[2].innerText &&
+    elements[0].innerText.length !== 0
+  ) {
+    elements[0].style="background-color: red";
+    elements[1].style="background-color: red";
+    elements[2].style="background-color: red";
+  }
+}
+
 function mark(element) {
-    element.innerText = "X";
+  if (element.innerText.length === 0) {
+    element.innerText = gammer;
+    winner();
+    if (gammer === "X") {
+      gammer = "O";
+    } else {
+      gammer = "X";
+    }
+  }
 }
